bafcd711c70ef7b87172b03a7be07bb09cfe2e9e
Yorl <94957768+DataFromBelow@users.noreply.github.com>
2026-04-10 13:33:17 +0200

    Deleting duplicates

--- Cobol/exercise1/Hello.cob
+++
@@ -1,9 +0,0 @@
-       identification division.
-       program-id HELLO.
-       data division.
-       working-storage section.
-       procedure division.
-       
-       display "HELLO, WORLD!"
-       stop run.
-       
